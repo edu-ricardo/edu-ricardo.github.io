429bd3e961199ef16961f7be894cec3b4e8bd380
eduardo.florencio <eduardo.florencio@linx.com.br>
2025-11-21 16:03:48 -0300

    Add footer with social links and enhance project display styling

--- src/main.ts
+++ src/main.ts
@@ -85,7 +85,25 @@ const renderProjetosNoAr = () => {
       <p>Links para aplicações e sistemas que estão disponíveis online.</p>
     </header>
     <main>
-      <ul>
+      <ul id="live-projects">
+        <li>
+          <a href="https://edu-ricardo.github.io/wiki-rpg/" target="_blank">Wiki RPG</a>
+          <p>Uma Wiki com informações de Lore do meu grupo de RPG</p>
+          <div class="tags">
+            <span class="tag">Web App</span>
+            <span class="tag">RPG</span>
+            <span class="tag">dnd</span>
+          </div>
+        </li>
+        <li>
+          <a href="https://rpg-calendario-b4d5e.firebaseapp.com/" target="_blank">Agendador de Sessões de RPG</a>
+          <p>Uma aplicação para meu grupo conseguir encontrar os melhores dias para uma sessão de RPG</p>
+          <div class="tags">
+            <span class="tag">Web App</span>
+            <span class="tag">RPG</span>
+            <span class="tag">dnd</span>
+          </div>
+        </li>
         <!-- Adicione seus links aqui -->
         <!-- <li><a href="URL_DO_PROJETO" target="_blank">Nome do Projeto</a> - Descrição breve.</li> -->
       </ul>
